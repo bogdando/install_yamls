26aff9e191ee10f61056a70feff55155414a15af
Bohdan Dobrelia <bdobreli@redhat.com>
2024-05-09 14:47:05 +0200

    Add multi-cell tripleo setup for adoption devel
    In order to keep the HW requirments for development
    of multi-cell OSP 17.1 adoption for RHOSO 18, provide
    a reduced multi-stack footprint
    (which is supported in tripleo, yet not in OSP):
    
    undercloud: 1 VM
    overcloud: Controller0 ( 1 VM, no HA)
    cell1: Compute0, Compute1 (CellController1) ( 2 VMs)
    cell2: Compute2+CellController2 (AIO VM host)
    
    No Ceph/HCI support yet, TBD.
    Only a fixed number of cells (2 extra cells) supported yet.
    
    Add partial local dev envs support (given VMs preprovisioned):
    * Fix HOME paths to match the default user home from ssh opts
      (when it is 'zuul', the commands fail on the control node,
       when executed from another user name)
    * Add missing repo setup commands and notes from standalone.sh
    * Support RH registry auth env vars
    * Disable undercloud install validations (to allow deployments
      on low storage envs)
    * Fix tripleo network config to let it configure nodes in a local
      setup, where there is no pre-configured networks.
    
    Add EDPM_CONFIGURE_NETWORKING to control tripleo networking
    configuration. Disable it for CI jobs. Should be enabled for
    local deployments.
    
    Add j2 bool filter support to jinja_render common func.
    
    Signed-off-by: Bohdan Dobrelia <bdobreli@redhat.com>

--- devsetup/scripts/tripleo.sh
+++ devsetup/scripts/tripleo.sh
@@ -85,10 +85,19 @@ sudo dnf install -y podman python3-tripleoclient util-linux lvm2
 sudo hostnamectl set-hostname undercloud.${CLOUD_DOMAIN}
 sudo hostnamectl set-hostname undercloud.${CLOUD_DOMAIN} --transient
 
+cat >\$HOME/nopingtests.yaml <<__EOF__
+parameter_defaults:
+    ValidateControllersIcmp: false
+    ValidateGatewaysIcmp: false
+    PingTestGatewayIPsMap: false
+    PingTestIpsMap: false
+__EOF__
+
 export HOST_PRIMARY_RESOLV_CONF_ENTRY=${HOST_PRIMARY_RESOLV_CONF_ENTRY}
 export INTERFACE_MTU=${INTERFACE_MTU:-1500}
 export NTP_SERVER=${NTP_SERVER:-"pool.ntp.org"}
 export IP=${IP}
+export IP_ADRESS_SUFFIX=${IP_ADRESS_SUFFIX}
 export GATEWAY=${GATEWAY}
 export EDPM_CONFIGURE_HUGEPAGES=${EDPM_CONFIGURE_HUGEPAGES:-false}
 export EDPM_COMPUTE_CEPH_ENABLED=${COMPUTE_CEPH_ENABLED:-false}
@@ -248,6 +257,9 @@ scp $SSH_OPT ${SCRIPTPATH}/../tripleo/tripleo_install.sh zuul@$IP:tripleo_instal
 scp $SSH_OPT ${SCRIPTPATH}/../tripleo/hieradata_overrides_undercloud.yaml zuul@$IP:hieradata_overrides_undercloud.yaml
 scp $SSH_OPT ${SCRIPTPATH}/../tripleo/undercloud-parameter-defaults.yaml zuul@$IP:undercloud-parameter-defaults.yaml
 scp $SSH_OPT ${MY_TMP_DIR}/undercloud.conf zuul@$IP:undercloud.conf
+scp $SSH_OPT ${MY_TMP_DIR}/vips_data0.yaml zuul@$IP:vips_data.yaml
+scp $SSH_OPT ${MY_TMP_DIR}/network_data0.yaml zuul@$IP:network_data.yaml
+scp $SSH_OPT ${SCRIPTPATH}/../tripleo/config-download-networker.yaml zuul@$IP:config-download-networker.yaml
 scp $SSH_OPT ${SCRIPTPATH}/../tripleo/nova_noceph.yaml zuul@$IP:nova_noceph.yaml
 scp $SSH_OPT ${SCRIPTPATH}/../tripleo/hugepages.yaml zuul@$IP:hugepages.yaml
 if [ $EDPM_COMPUTE_CELLS -gt 1 ]; then
@@ -257,6 +269,7 @@ if [ $EDPM_COMPUTE_CELLS -gt 1 ]; then
         scp $SSH_OPT ${MY_TMP_DIR}/overcloud_services_cell${cell}.yaml zuul@$IP:overcloud_services_cell${cell}.yaml
         scp $SSH_OPT ${MY_TMP_DIR}/config-download-cell${cell}.yaml zuul@$IP:config-download-cell${cell}.yaml
     done
+    scp $SSH_OPT ${SCRIPTPATH}/../tripleo/config-download-multistack.yaml zuul@$IP:config-download-multistack.yaml
 else
     scp $SSH_OPT ${SCRIPTPATH}/../tripleo/vips_data.yaml zuul@$IP:vips_data.yaml
     scp $SSH_OPT ${MY_TMP_DIR}/network_data.yaml zuul@$IP:network_data.yaml
